4f5df423616da439cc90cecbbb0b41b82bfd1cfd
Aman <aman@thewelzin.com>
2025-06-23 16:26:17 +0530

    prod push 3

---
+++ webapp/backend/routes/cardroutes.js
@@ -0,0 +1,27 @@
+const express= require("express")
+const router= express.Router()
+
+const {verifyToken} = require("../middlewares/verifyToken")
+const {createCardGroup,addUserToGroup,getAllCard,
+    getDistinctGroupsForUser,getGroupWithMembersAndCards,
+    deleteMember,leaveGroup,deleteGroup
+} = require("../controller/cardpool")
+
+const {Cardfetch, recommended_card}=require("../controller/Card/cardfetch")
+const{all_bank} = require("../controller/Card/cardfetch")
+// const { verify } = require("jsonwebtoken")
+router.post("/your_recomendation",Cardfetch)
+
+router.get("/all_bank",all_bank)
+router.get("/recommendedcard",verifyToken,recommended_card)
+router.post("/createPool",verifyToken, createCardGroup)
+router.post("/addUserToPool",verifyToken,addUserToGroup)
+router.get("/getAllUserCard",verifyToken,getAllCard)
+router.get("/getDistinctGroupsForUser",verifyToken,getDistinctGroupsForUser)
+router.get("/getGroupWithMembersAndCards/:groupId",verifyToken,getGroupWithMembersAndCards)
+router.post("/removeUserFromPool",verifyToken,deleteMember )
+router.post("/leaveGroup",verifyToken,leaveGroup)
+router.delete("/deletePool/:groupId", verifyToken, deleteGroup); 
+// router.post("/",Cardfetch)
+
+module.exports= router

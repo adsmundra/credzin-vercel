be6b54d975175ead35b8631bcc16ab51049f41af
Aman <aman@thewelzin.com>
2025-06-24 13:48:08 +0530

    prod push

--- webapp/backend/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {editProfileController} = require('../controller/profileController.js');
-const uploadMiddleware = require('..//middlewares/uploadMiddleware.js');
-
-router.put('/profileEdit', uploadMiddleware, editProfileController);
-
-module.exports = router;
